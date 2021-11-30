bbc9900f9a1d54085a622eebdb329d38f1dd7ef5
sakilanasrinsetu <setusakilanasrin@gmail.com>
2021-11-30 14:52:26 +0600

    variable type

--- variable.go
+++ variable.go
@@ -2,6 +2,13 @@ package main
 import "fmt"
 
 func main(){
-	c:=33
-	fmt.Printf("%T, %v\n", c,c)
+	a := "Hello"
+	b := 33
+	c := 2.4
+	d := true
+	fmt.Printf("\nType of A is: %T", a)
+	fmt.Printf("\nType of B is: %T", b)
+	fmt.Printf("\nType of C is: %T", c)
+	fmt.Printf("\nType of D is: %T", d)
+	fmt.Printf("\n")
 }
