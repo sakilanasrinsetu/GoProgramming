2a689090825433c2eb7859fba92fdf786dfa6f35
sakilanasrinsetu <setusakilanasrin@gmail.com>
2021-11-30 14:41:01 +0600

    data variable

---
+++ calculation.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var a int = 12
+	var b int = 10
+	fmt.Println("The Value of A is = ", a,"\nThe Value of B is = ", b,
+	"\nThe Value of A + B is = ", a+b, "\nThe Value of A - B is = ", a-b,
+	"\nThe Value of A * B is = ", a*b,"\nThe Value of A / B is = ", a/b,
+	"\nThe Value of A % B is = ", a%b)
+}
--- variable.go
+++ variable.go
@@ -1,12 +1,7 @@
 package main
-
 import "fmt"
 
-func main()  {
-	var a int = 12
-	var b int = 10
-	fmt.Println("The Value of A is = ", a,"\nThe Value of B is = ", b,
-	"\nThe Value of A + B is = ", a+b, "\nThe Value of A - B is = ", a-b,
-	"\nThe Value of A * B is = ", a*b,"\nThe Value of A / B is = ", a/b,
-	"\nThe Value of A % B is = ", a%b)
+func main(){
+	c:=33
+	fmt.Printf("%T, %v\n", c,c)
 }
