30c0f91365151f96debfa5a785e03b8204c44e70
sakilanasrinsetu <setusakilanasrin@gmail.com>
2021-11-30 14:07:24 +0600

    variable + oparators

---
+++ variable.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var a int = 12
+	var b int = 10
+	fmt.Println("The Value of A is = ", a)
+	fmt.Println("The Value of B is = ", b)
+	fmt.Println("The Value of A + B is = ", a+b)
+	fmt.Println("The Value of A - B is = ", a-b)
+	fmt.Println("The Value of A * B is = ", a*b)
+	fmt.Println("The Value of A / B is = ", a/b)
+	fmt.Println("The Value of A % B is = ", a%b)
+}
