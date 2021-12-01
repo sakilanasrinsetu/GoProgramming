a0d600d9046b8fb031d40280bd514185c4c192bd
sakilanasrinsetu <setusakilanasrin@gmail.com>
2021-12-01 17:47:17 +0600

    add

--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main 
+
 import (
-	"net/http"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"fmt"
 )
 
  func main()  {
-	 http.HandleFunc("/", func(http.ResponseWriter, *http.Request){
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		 log.Println("Hello World")
+		//  d, _ := ioutil.ReadAll(r.Body)
+
+		d, err := ioutil.ReadAll(r.Body)
+		
+		if err != nil {
+			// rw.WriteHeader(http.StatusBadRequest)
+			// rw.Write([]byte("Ooops"))
+			// return
+			http.Error(rw, "Opps", http.StatusBadRequest)
+			return
+		}
+
+		log.Printf("Data %s\n", d)
+        fmt.Fprintf(rw, "Hello %s", d)
 	 })
+
 	 http.HandleFunc("/goodebye/", func(http.ResponseWriter, *http.Request){
 		log.Println("Goodbye")
 	})
